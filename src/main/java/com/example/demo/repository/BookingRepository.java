dada44458117f490ab889d663cdf144fca4a1ed2
Chryzor <chryzor@github.com>
2026-03-24 13:48:58 -0400

    Added the proper frameworks

--- src/main/java/com/example/demo/repository/BookingRepository.java
+++ src/main/java/com/example/demo/repository/BookingRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.example.demo.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
